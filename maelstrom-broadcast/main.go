4bf1aa1455cc33c91951428e175005cbcb8d36c3
Yash Gulati <yashgulati@Yashs-MacBook-Air.local>
2025-03-09 17:07:40 +0530

    Challenge #3b: Multi-Node Broadcast

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"sync"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
@@ -10,7 +11,9 @@ import (
 func main() {
 	log.Println("Initializing node...")
 
-	message_ids := make(map[any]bool)
+	var message_ids sync.Map
+	var unsent_message_ids sync.Map
+	nodes := []string{"n0", "n1", "n2", "n3", "n4"}
 
 	n := maelstrom.NewNode()
 
@@ -22,21 +25,43 @@ func main() {
 			return err
 		}
 
-		// store the message ID
-		message_ids[body["message"]] = true
+		log.Println(body)
 
-		response := map[string]any{"type": "broadcast_ok"}
-		return n.Reply(msg, response)
+		// store the message ID
+		message_ids.Store(body["message"], true)
+		unsent_message_ids.Store(body["message"], true)
+
+		if val, ok := body["isRebroadcast"]; !ok || val == false {
+			log.Println("Send re-broadcast message to all nodes")
+
+			response := map[string]any{"type": "broadcast_ok"}
+			n.Reply(msg, response)
+
+			for _, next_node := range nodes {
+				unsent_message_ids.Range(func(unsent_message_id, _ interface{}) bool {
+					rebroadcast_request := map[string]any{"type": "broadcast", "message": unsent_message_id, "isRebroadcast": true}
+					n.Send(next_node, rebroadcast_request)
+					return true
+				})
+			}
+
+			unsent_message_ids = sync.Map{}
+
+		}
+
+		return nil
+
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
 		log.Println("Handling read request")
 
 		// Convert stored message IDs into a slice
-		message_ids_arr := make([]any, 0, len(message_ids))
-		for k := range message_ids {
+		var message_ids_arr []any
+		message_ids.Range(func(k, v interface{}) bool {
 			message_ids_arr = append(message_ids_arr, k)
-		}
+			return true
+		})
 
 		response := map[string]any{
 			"type":     "read_ok",
